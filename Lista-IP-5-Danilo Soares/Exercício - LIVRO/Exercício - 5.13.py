bba95d99b8b522c7d3b49c39fb1bfbcc5b0c8dc3
Danilo Soares <danilo.soares@dce.ufpb.br>
2017-08-14 22:09:30 -0300

    Lista - IP

--- Lista-IP-5-Danilo Soares/Exercício - LIVRO/Exercício - 5.13.py
+++ Lista-IP-5-Danilo Soares/Exercício - LIVRO/Exercício - 5.13.py
@@ -1,3 +1,23 @@
-numb= int(input("Oi = "))
+divida=(float(input("Valor da dívida = ")))
+jurosmensal=(float(input("Juros mensal = ")))
+valorpg=(float(input("Valor pago por mês = ")))
 
-print(if numb > 0: "Oi")
+jurosfinal= jurosmensal/100
+
+x=0
+soma=0
+
+while x <= 23:
+
+
+
+
+
+
+
+
+    #res= (divida * jurosfinal) + divida
+    #soma = res
+    #x += 1
+    #valor= res - valorpg
+        
--- Lista-IP-5-Danilo Soares/Exercício - LIVRO/Exercício - 5.6.py
+++ Lista-IP-5-Danilo Soares/Exercício - LIVRO/Exercício - 5.6.py
@@ -1,9 +1,7 @@
 n = int(input("Tabuada de :"))
-
 x = 1
 
 while x <= 10:
-     print("{} * {} = " .format(n,x),end="")
-     print(n * x)
-     
-     x = x + 1
+     res= n*x
+     print("{} * {} = {}" .format(n,x,res))
+     x += 1
