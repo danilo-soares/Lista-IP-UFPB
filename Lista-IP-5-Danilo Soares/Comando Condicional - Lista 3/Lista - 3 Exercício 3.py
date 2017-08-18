68da3ea2cfe81c7671d994e852ed327916b20d93
danilo-soares <danilo.soares@dce.ufpb.br>
2017-08-18 14:04:56 -0300

    Lista-IP

--- Lista-IP-5-Danilo Soares/Comando Condicional - Lista 3/Lista - 3 Exercício 3.py
+++ Lista-IP-5-Danilo Soares/Comando Condicional - Lista 3/Lista - 3 Exercício 3.py
@@ -1,45 +1,24 @@
-quantidade=(int(input("Digite a quantidade de pessoas = ")))
+quantidade= (int(input("Digite a quantidade de pessoas = ")))
+totalpessoas = quantidade
 
-onibus= 42
-van= 20
+cpbus= 42
+cpvan= 20
+
 precobus= 350
 precovan= 200
 
-if(quantidade <= 41 ):
- 
-    if(quantidade <= 20):
-        lotvan= 1
-        lotbus= 0
 
-    else:
-        lotvan= 2
-        lotbus= 0
+nbus = totalpessoas // cpbus
+numpessoas = totalpessoas % cpbus
 
-    valorvan= lotvan * precovan
-    valorbus= 0
-    pgvan= valorvan / quantidade
-    pgbus= 0
+nvans = numpessoas // cpvan
+numpessoas = numpessoas % cpvan
 
-else :
-    
-    if(quantidade == 42):
-        lotbus= 1
-        lotvan= 0
+if numpessoas > 0:
+    nvans += 1
 
-    elif(quantidade <= 52):
-        lotbus= quantidade // onibus
-        lotvan= 1
-        
-    else:
-        lotbus= quantidade // onibus
-        lotvan= round(((quantidade % onibus)/van))
+print("{} onibus e {} vans" .format(nbus,nvans))
 
-    valorvan= lotvan * precovan
-    valorbus= lotbus * precobus
-    
-pgvan= valorvan / quantidade
-pgbus= valorbus / quantidade 
-valorfinal= pgvan + pgbus         
+valortotal =(int)((nbus*precobus) + (nvans*precovan))/totalpessoas
 
-print("{} ônibus e {} van(s)\nR$ {:.2f} por pessoa." .format(lotbus,lotvan,valorfinal))
-
+print("R$ {:.2f} por pessoa" .format(valortotal))
