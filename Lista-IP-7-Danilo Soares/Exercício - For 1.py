051c77acce855ff06ca75d8fee3613601d5cf266
danilo-soares <danilo.soares@dce.ufpb.br>
2017-08-27 23:36:45 -0300

    Lista-IP

---
+++ Lista-IP-7-Danilo Soares/Exercício - For 1.py
@@ -0,0 +1,26 @@
+quantidade=int(input("Digite a quantidade de produtos -> "))
+cont=0
+valorfinal=0
+maior=0
+
+for i in range(quantidade):
+
+    descricao=(input("Descrição do produto -> "))
+    valor=int(input("Digite o valor do produto -> "))
+    ano=int(input("Digite o ano do produto -> "))
+
+    if ano < 1827:
+        cont += 1
+
+    if maior > valor:
+
+        maior = valor
+        anob= ano
+        descricaob=descricao
+
+    valorfinal += valor
+
+preco = valorfinal / quantidade
+print("Itens produzidos antes de 1827 -> {}" .format(cont))
+print("Valor médio dos itens -> {:.2f}" .format(preco))
+print("Dados do objeto mais valioso -> {},{}" .format(descricaob,anob))
